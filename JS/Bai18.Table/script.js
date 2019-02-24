f7aa6272c60a7099bfde7c2f96f877eeb6fcf3a7
jessen <ndt94hvnh@gmail.com>
2019-02-24 21:31:53 +0700

    Bai18

---
+++ JS/Bai18.Table/script.js
@@ -0,0 +1,8 @@
+function sortTable() {
+    // obj = $('.idol').text();
+    // console.log(obj);
+    // arr = Array.from(obj);
+    // console.log(arr);
+    obj = $('.idol').text().split();
+    console.log(obj);
+}
