6124991613693357e8cc24640419816d6a0bc67d
jessen <ndt94hvnh@gmail.com>
2019-05-16 22:16:08 +0700

    fix index page
    using js to render index page

--- AW/js/main.js
+++ AW/js/main.js
@@ -1,3 +1,4 @@
+// FUNCTION TO RENDER HEADER + FOOTER
 function includeHTML() {
 	var z, i, elmnt, file, xhttp;
 	/* Loop through a collection of all HTML elements: */
@@ -30,142 +31,136 @@ function includeHTML() {
 	}
 }
 
+// DATA FOR RENDERING
 let watches = [
 	{
-		name: 'name1',
-		price: '20.000.000'
+		id: 0,
+		name: 'CARTIER TANK LOUIS',
+		price: '20.000.000',
+		image:
+			'<img src="img/index/VACHERON-CONSTANTIN-22-03-14-19-1-850x566.png" class="img-fluid" alt="cartier_tank_louis">'
 	},
 	{
-		name: 'name2',
-		price: '20.000.000'
+		id: 1,
+		name: 'VACHERON CONSTANTIN 222',
+		price: '20.000.000',
+		image:
+			'<img src="img/index/AP-Rolyal-Oak-03-12-19-1-450x300.png" class="img-fluid" alt="vacheron">'
 	},
 	{
-		name: 'name3',
-		price: '20.000.000'
+		id: 2,
+		name: 'AUDEMARS PIGUET ROYAL OAK 14790',
+		price: '20.000.000',
+		image:
+			'<img src="img/index/CARTIER-MUST-DE-TANK-02-16-19-1-450x300.png" class="img-fluid" alt="audemars">'
 	},
 	{
-		name: 'name4',
-		price: '20.000.000'
+		id: 3,
+		name: 'CARTIER “STEPPED” TANK',
+		price: '20.000.000',
+		image:
+			'<img src="img/index/CARTIER-TANK-BLUE-01-29-19-1-450x300.png" class="img-fluid" alt="cartier_stepped_tank">'
 	},
 	{
-		name: 'name5',
-		price: '20.000.000'
+		id: 4,
+		name: 'OMEGA SPEEDMASTER CO-AXIAL',
+		price: '20.000.000',
+		image:
+			'<img src="img/index/OMEGA-SPEEDMASTER-COAXIAL-CHRONO-02-16-19-1-450x300.png" class="img-fluid" alt="omega">'
 	},
 	{
-		name: 'name6',
-		price: '20.000.000'
+		id: 5,
+		name: 'ROLEX DAY DATE REF. 1803',
+		price: '20.000.000',
+		image:
+			'<img src="img/index/ROLEX-DATEJUST-1801-YG-02-16-19-1-450x300.png" class="img-fluid" alt="rolex_1803">'
 	},
 	{
-		name: 'name7',
-		price: '20.000.000'
+		id: 6,
+		name: 'UNIVERSAL GENEVE POLEROUTER',
+		price: '20.000.000',
+		image:
+			'<img src="img/index/UNIVERSAL-GENEVE-GOLD-12-25-18-1-450x300.png" class="img-fluid" alt="universal_geneve">'
 	},
 	{
-		name: 'name8',
-		price: '20.000.000'
+		id: 7,
+		name: 'OMEGA SUVERAN',
+		price: '20.000.000',
+		image:
+			'<img src="img/index/OMEGA-PATINA-01-02-19-1-450x300.png" class="img-fluid" alt="omega_patina">'
 	},
 	{
-		name: 'name9',
-		price: '20.000.000'
+		id: 8,
+		name: 'OMEGA GENEVE DYNAMIC',
+		price: '20.000.000',
+		image:
+			'<img src="img/index/Omega-Geneve-Dynamic-12-04-18-1-450x300.png" class="img-fluid" alt="omega_geneve">'
 	}
 ];
 
-$('#product0').on('click', function() {
-	product0 = localStorage.setItem('watches0', JSON.stringify(watches[0]));
-	console.log(product0);
-});
+// THREE FUNCTION TO RENDER INDEX PAGE
+function renderContentRow1() {
+	let htmlContent = '';
+	for (var i = 0; i < 3; i++) {
+		htmlContent += `
+		<div class="col-md-4 text-center">
+			${watches[i].image}
+			<p class="products-name">${watches[i].name}</p>
+			<p class="products-price">${watches[i].price}</p>
+			<a id="product${i}" href="cart.html" class="products-btn products-btn-buy btn btn-primary" onclick="renderContentById(${i})">Mua ngay</a>
+			<button type="button" class="products-btn products-btn-more btn btn-primary">Xem thêm</button>
+		</div>
+	`;
+		$('.row1').html(htmlContent);
+	}
+}
 
-$('#product1').on('click', function() {
-	product1 = localStorage.setItem('watches1', JSON.stringify(watches[1]));
-	console.log(product1);
-});
+function renderContentRow2() {
+	let htmlContent = '';
+	for (var i = 3; i < 6; i++) {
+		htmlContent += `
+		<div class="col-md-4 text-center">
+			${watches[i].image}
+			<p class="products-name">${watches[i].name}</p>
+			<p class="products-price">${watches[i].price}</p>
+			<a id="product${i}" href="cart.html" class="products-btn products-btn-buy btn btn-primary" onclick="renderContentById(${i})">Mua ngay</a>
+			<button type="button" class="products-btn products-btn-more btn btn-primary">Xem thêm</button>
+		</div>
+	`;
+		$('.row2').html(htmlContent);
+	}
+}
 
-$('#product2').on('click', function() {
-	product2 = localStorage.setItem('watches2', JSON.stringify(watches[2]));
-	console.log(product2);
-});
+function renderContentRow3() {
+	let htmlContent = '';
+	for (var i = 6; i < 9; i++) {
+		htmlContent += `
+		<div class="col-md-4 text-center">
+			${watches[i].image}
+			<p class="products-name">${watches[i].name}</p>
+			<p class="products-price">${watches[i].price}</p>
+			<a id="product${i}" href="cart.html" class="products-btn products-btn-buy btn btn-primary" onclick="renderContentById(${i})">Mua ngay</a>
+			<button type="button" class="products-btn products-btn-more btn btn-primary">Xem thêm</button>
+		</div>
+	`;
+		$('.row3').html(htmlContent);
+	}
+}
 
-$('#product3').on('click', function() {
-	product3 = localStorage.setItem('watches3', JSON.stringify(watches[3]));
-	console.log(product3);
-});
-
-$('#product4').on('click', function() {
-	product4 = localStorage.setItem('watches4', JSON.stringify(watches[4]));
-	console.log(product4);
-});
-
-$('#product5').on('click', function() {
-	product5 = localStorage.setItem('watches5', JSON.stringify(watches[5]));
-	console.log(product5);
-});
-
-$('#product6').on('click', function() {
-	product6 = localStorage.setItem('watches6', JSON.stringify(watches[6]));
-	console.log(product6);
-});
-
-$('#product7').on('click', function() {
-	product7 = localStorage.setItem('watches7', JSON.stringify(watches[7]));
-	console.log(product7);
-});
-
-$('#product8').on('click', function() {
-	product8 = localStorage.setItem('watches8', JSON.stringify(watches[8]));
-	console.log(product8);
-});
-
+// FUNCTION TO RENDER CART ITEM
 let empty_arr = [];
-info_parse_1 = JSON.parse(localStorage.getItem('watches0'));
-info_parse_2 = JSON.parse(localStorage.getItem('watches1'));
-info_parse_3 = JSON.parse(localStorage.getItem('watches2'));
-info_parse_4 = JSON.parse(localStorage.getItem('watches3'));
-info_parse_5 = JSON.parse(localStorage.getItem('watches4'));
-info_parse_6 = JSON.parse(localStorage.getItem('watches5'));
-info_parse_7 = JSON.parse(localStorage.getItem('watches6'));
-info_parse_8 = JSON.parse(localStorage.getItem('watches7'));
-info_parse_9 = JSON.parse(localStorage.getItem('watches8'));
-
-empty_arr.push(info_parse_1);
-empty_arr.push(info_parse_2);
-empty_arr.push(info_parse_3);
-empty_arr.push(info_parse_4);
-empty_arr.push(info_parse_5);
-empty_arr.push(info_parse_6);
-empty_arr.push(info_parse_7);
-empty_arr.push(info_parse_8);
-empty_arr.push(info_parse_9);
+localStorage.setItem('watches', JSON.stringify(watches));
+info = JSON.parse(localStorage.getItem('watches'));
+console.log(info);
 
 console.log(empty_arr);
 
-function renderContent() {
-	let htmlContent = '';
-	empty_arr.forEach(function(element) {
-		htmlContent += `
-			<div class="col-md-3">
-				<p>Sản phẩm</p>
-				<img src="img/index/OMEGA-PATINA-01-02-19-1-450x300.png" class="img-fluid">
-				<p>${element.name}</p>
-			</div>
-
-			<div class="col-md-3">
-				<p>Giá</p>
-				<p id='test'>${element.price}</p>
-			</div>
-
-			<div class="col-md-3">
-				<p>Số lượng</p>
-				<p>1</p>
-			</div>
-
-			<div class="col-md-2">
-				<p>Tổng</p>
-				<p>20.000.000</p>
-			</div>
-
-			<div class="col-md-1">
-				<i class="fas fa-trash"></i>
-			</div>
-		`;
-		$('.cart .row').append(htmlContent);
-	});
+function pushProductInfo() {
+	for (var i = 0; i < 9; i++) {
+		$('#product' + i).on('click', function() {
+			empty_arr.push(info[i]);
+		});
+	}
 }
+
+function renderContentById() {}
