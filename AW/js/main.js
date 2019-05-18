ceea180d5d3cdfea8b7b0049470ba6ee1f241f2e
jessen <ndt94hvnh@gmail.com>
2019-05-18 16:56:45 +0700

    fix index + cart page

--- AW/js/main.js
+++ AW/js/main.js
@@ -1,108 +1,108 @@
 // FUNCTION TO RENDER HEADER + FOOTER
 function includeHTML() {
-	var z, i, elmnt, file, xhttp;
-	/* Loop through a collection of all HTML elements: */
-	z = document.getElementsByTagName('*');
-	for (i = 0; i < z.length; i++) {
-		elmnt = z[i];
-		/*search for elements with a certain atrribute:*/
-		file = elmnt.getAttribute('w3-include-html');
-		if (file) {
-			/* Make an HTTP request using the attribute value as the file name: */
-			xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function() {
-				if (this.readyState == 4) {
-					if (this.status == 200) {
-						elmnt.innerHTML = this.responseText;
-					}
-					if (this.status == 404) {
-						elmnt.innerHTML = 'Page not found.';
-					}
-					/* Remove the attribute, and call this function once more: */
-					elmnt.removeAttribute('w3-include-html');
-					includeHTML();
-				}
-			};
-			xhttp.open('GET', file, true);
-			xhttp.send();
-			/* Exit the function: */
-			return;
-		}
-	}
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName('*');
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute('w3-include-html');
+        if (file) {
+            /* Make an HTTP request using the attribute value as the file name: */
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = 'Page not found.';
+                    }
+                    /* Remove the attribute, and call this function once more: */
+                    elmnt.removeAttribute('w3-include-html');
+                    includeHTML();
+                }
+            };
+            xhttp.open('GET', file, true);
+            xhttp.send();
+            /* Exit the function: */
+            return;
+        }
+    }
 }
 
 // DATA FOR RENDERING
 let watches = [
-	{
-		id: 0,
-		name: 'CARTIER TANK LOUIS',
-		price: '20.000.000',
-		image:
-			'<img src="img/index/VACHERON-CONSTANTIN-22-03-14-19-1-850x566.png" class="img-fluid" alt="cartier_tank_louis">'
-	},
-	{
-		id: 1,
-		name: 'VACHERON CONSTANTIN 222',
-		price: '20.000.000',
-		image:
-			'<img src="img/index/AP-Rolyal-Oak-03-12-19-1-450x300.png" class="img-fluid" alt="vacheron">'
-	},
-	{
-		id: 2,
-		name: 'AUDEMARS PIGUET ROYAL OAK 14790',
-		price: '20.000.000',
-		image:
-			'<img src="img/index/CARTIER-MUST-DE-TANK-02-16-19-1-450x300.png" class="img-fluid" alt="audemars">'
-	},
-	{
-		id: 3,
-		name: 'CARTIER “STEPPED” TANK',
-		price: '20.000.000',
-		image:
-			'<img src="img/index/CARTIER-TANK-BLUE-01-29-19-1-450x300.png" class="img-fluid" alt="cartier_stepped_tank">'
-	},
-	{
-		id: 4,
-		name: 'OMEGA SPEEDMASTER CO-AXIAL',
-		price: '20.000.000',
-		image:
-			'<img src="img/index/OMEGA-SPEEDMASTER-COAXIAL-CHRONO-02-16-19-1-450x300.png" class="img-fluid" alt="omega">'
-	},
-	{
-		id: 5,
-		name: 'ROLEX DAY DATE REF. 1803',
-		price: '20.000.000',
-		image:
-			'<img src="img/index/ROLEX-DATEJUST-1801-YG-02-16-19-1-450x300.png" class="img-fluid" alt="rolex_1803">'
-	},
-	{
-		id: 6,
-		name: 'UNIVERSAL GENEVE POLEROUTER',
-		price: '20.000.000',
-		image:
-			'<img src="img/index/UNIVERSAL-GENEVE-GOLD-12-25-18-1-450x300.png" class="img-fluid" alt="universal_geneve">'
-	},
-	{
-		id: 7,
-		name: 'OMEGA SUVERAN',
-		price: '20.000.000',
-		image:
-			'<img src="img/index/OMEGA-PATINA-01-02-19-1-450x300.png" class="img-fluid" alt="omega_patina">'
-	},
-	{
-		id: 8,
-		name: 'OMEGA GENEVE DYNAMIC',
-		price: '20.000.000',
-		image:
-			'<img src="img/index/Omega-Geneve-Dynamic-12-04-18-1-450x300.png" class="img-fluid" alt="omega_geneve">'
-	}
+    {
+        id: 0,
+        name: 'CARTIER TANK LOUIS',
+        price: '20.000.000',
+        image:
+            '<img src="img/index/VACHERON-CONSTANTIN-22-03-14-19-1-850x566.png" class="img-fluid" alt="cartier_tank_louis">'
+    },
+    {
+        id: 1,
+        name: 'VACHERON CONSTANTIN 222',
+        price: '20.000.000',
+        image:
+            '<img src="img/index/AP-Rolyal-Oak-03-12-19-1-450x300.png" class="img-fluid" alt="vacheron">'
+    },
+    {
+        id: 2,
+        name: 'AUDEMARS PIGUET ROYAL OAK 14790',
+        price: '20.000.000',
+        image:
+            '<img src="img/index/CARTIER-MUST-DE-TANK-02-16-19-1-450x300.png" class="img-fluid" alt="audemars">'
+    },
+    {
+        id: 3,
+        name: 'CARTIER “STEPPED” TANK',
+        price: '20.000.000',
+        image:
+            '<img src="img/index/CARTIER-TANK-BLUE-01-29-19-1-450x300.png" class="img-fluid" alt="cartier_stepped_tank">'
+    },
+    {
+        id: 4,
+        name: 'OMEGA SPEEDMASTER CO-AXIAL',
+        price: '20.000.000',
+        image:
+            '<img src="img/index/OMEGA-SPEEDMASTER-COAXIAL-CHRONO-02-16-19-1-450x300.png" class="img-fluid" alt="omega">'
+    },
+    {
+        id: 5,
+        name: 'ROLEX DAY DATE REF. 1803',
+        price: '20.000.000',
+        image:
+            '<img src="img/index/ROLEX-DATEJUST-1801-YG-02-16-19-1-450x300.png" class="img-fluid" alt="rolex_1803">'
+    },
+    {
+        id: 6,
+        name: 'UNIVERSAL GENEVE POLEROUTER',
+        price: '20.000.000',
+        image:
+            '<img src="img/index/UNIVERSAL-GENEVE-GOLD-12-25-18-1-450x300.png" class="img-fluid" alt="universal_geneve">'
+    },
+    {
+        id: 7,
+        name: 'OMEGA SUVERAN',
+        price: '20.000.000',
+        image:
+            '<img src="img/index/OMEGA-PATINA-01-02-19-1-450x300.png" class="img-fluid" alt="omega_patina">'
+    },
+    {
+        id: 8,
+        name: 'OMEGA GENEVE DYNAMIC',
+        price: '20.000.000',
+        image:
+            '<img src="img/index/Omega-Geneve-Dynamic-12-04-18-1-450x300.png" class="img-fluid" alt="omega_geneve">'
+    }
 ];
 localStorage.setItem('watches', JSON.stringify(watches));
 // THREE FUNCTION TO RENDER INDEX PAGE
 function renderContentRow1() {
-	let htmlContent = '';
-	for (var i = 0; i < 3; i++) {
-		htmlContent += `
+    let htmlContent = '';
+    for (var i = 0; i < 3; i++) {
+        htmlContent += `
 		<div class="col-md-4 text-center">
 			${watches[i].image}
 			<p class="products-name">${watches[i].name}</p>
@@ -111,14 +111,14 @@ function renderContentRow1() {
 			<a id="product_${i}" href="detail_product.html" class="products-btn products-btn-more btn btn-primary" onclick="transferId(${i})">Xem thêm</a>
 		</div>
 	`;
-		$('.row1').html(htmlContent);
-	}
+        $('.row1').html(htmlContent);
+    }
 }
 
 function renderContentRow2() {
-	let htmlContent = '';
-	for (var i = 3; i < 6; i++) {
-		htmlContent += `
+    let htmlContent = '';
+    for (var i = 3; i < 6; i++) {
+        htmlContent += `
 		<div class="col-md-4 text-center">
 			${watches[i].image}
 			<p class="products-name">${watches[i].name}</p>
@@ -127,14 +127,14 @@ function renderContentRow2() {
 			<a id="product_${i}" href="detail_product.html" class="products-btn products-btn-more btn btn-primary" onclick="transferId(${i})">Xem thêm</a>
 		</div>
 	`;
-		$('.row2').html(htmlContent);
-	}
+        $('.row2').html(htmlContent);
+    }
 }
 
 function renderContentRow3() {
-	let htmlContent = '';
-	for (var i = 6; i < 9; i++) {
-		htmlContent += `
+    let htmlContent = '';
+    for (var i = 6; i < 9; i++) {
+        htmlContent += `
 		<div class="col-md-4 text-center">
 			${watches[i].image}
 			<p class="products-name">${watches[i].name}</p>
@@ -143,29 +143,29 @@ function renderContentRow3() {
 			<a id="product_${i}" href="detail_product.html" class="products-btn products-btn-more btn btn-primary" onclick="transferId(${i})">Xem thêm</a>
 		</div>
 	`;
-		$('.row3').html(htmlContent);
-	}
+        $('.row3').html(htmlContent);
+    }
 }
 
 // FUNCTION TO RENDER CART ITEM
-// let empty_arr = [];
-// console.log(empty_arr);
-
-// function pushInfoById(id) {
-// 	empty_arr.push(watches[id]);
-// 	localStorage.setItem('info', JSON.stringify(empty_arr));
-// }
-
-// test = JSON.parse(localStorage.getItem('info'));
-// console.log(test);
-// console.log(empty_arr);
-
-// function renderContentById() {}
+function pushInfoById(id) {
+    let itemsArray = localStorage.getItem('item')
+        ? JSON.parse(localStorage.getItem('item'))
+        : [];
+    if (itemsArray.some(r => watches.includes(r)) === false) {
+        itemsArray.push(watches[id]);
+    }
+    // itemsArray.push(watches[id]);
+    localStorage.setItem('item', JSON.stringify(itemsArray));
+}
 
 // TRANSFER ID TO DETAIL_PRODUCT PAGE FOR RENDERING
 function transferId(id) {
-	productId = $('#product_' + id)
-		.attr('id')
-		.slice(-1);
-	localStorage.setItem('productId', productId);
+    productId = $('#product_' + id)
+        .attr('id')
+        .slice(-1);
+    localStorage.setItem('productId', productId);
 }
+
+// test = [{ id: 1, name: 'test1' }, { id: 2, name: 'test2' }];
+// test1 = [{ id: 1, name: 'test1' }, { id: 2, name: 'test2' }];
