9d53caeb4119f2c5f49d2d49dc7b8c7bea443b8d
jessen <ndt94hvnh@gmail.com>
2019-05-26 22:42:46 +0700

    update product page
    add sort by price feature and some css for mobile screen

--- AW/js/product.js
+++ AW/js/product.js
@@ -2,179 +2,187 @@ watches = JSON.parse(localStorage.getItem('watches'));
 
 // FUNCTION WHEN LOAD PAGE
 $(function() {
-    renderContent(watches);
-    sortByPriceAsc();
-    sortByPriceDesc();
-    sortByPriceNormal();
+	renderContent(watches);
+	sortByPriceAsc();
+	sortByPriceDesc();
+	sortByPriceNormal();
+	showFilter();
 });
 
 // FUNCTION TO FORMAT PRICE FROM STRING TO NUMBER
 function formatString(string) {
-    return parseInt(string.replace(/\./g, ''));
+	return parseInt(string.replace(/\./g, ''));
 }
 
 // FUNCTION TO RENDER CONTENT ON PRODUCTS RIGHT PART
 function renderContent(arr) {
-    let htmlContent = '';
-    arr.forEach(function(item, index) {
-        htmlContent += `
+	let htmlContent = '';
+	arr.forEach(function(item, index) {
+		htmlContent += `
         <div class="col-md-4 text-center">
             ${item.image}
 			<p class="products-name">${item.name}</p>
 			<p class="products-name">Year: ${item.year}</p>
             <p class="products-price">${item.price}</p>
-            <a href="cart.html" class="products-btn products-btn-buy btn btn-primary" onclick="pushInfoById(${index})">Mua ngay</a>
-            <a id="product_${index}" href="detail_product.html" class="products-btn products-btn-more btn btn-primary" onclick="transferId(${index})">Xem thêm</a>
+            <a href="cart.html" class="products-btn products-btn-buy btn btn-sm btn-primary" onclick="pushInfoById(${index})">Mua ngay</a>
+            <a id="product_${index}" href="detail_product.html" class="products-btn products-btn-more btn btn-sm btn-primary" onclick="transferId(${index})">Xem thêm</a>
         </div>
         `;
-    });
-    $('.products .col-md-9 .row').html(htmlContent);
+	});
+	$('.products .col-md-9 .row').html(htmlContent);
 }
 
 // FUNCTION TO RENDER CONTENT AFTER FILTER BY TYPE OF WATCH
 function renderContentByType(id) {
-    watch = [];
-    if ($('#' + id).prop('checked')) {
-        watches.forEach(function(item, index) {
-            if (item.type === id) {
-                watch.push(item);
-            }
-        });
-        renderContent(watch);
-    } else {
-        renderContent(watches);
-    }
+	watch = [];
+	if ($('#' + id).prop('checked')) {
+		watches.forEach(function(item, index) {
+			if (item.type === id) {
+				watch.push(item);
+			}
+		});
+		renderContent(watch);
+	} else {
+		renderContent(watches);
+	}
 }
 
 // FUNCTION TO RENDER CONTENT AFTER FILTER BY YEAR
 function renderContentByYear(year) {
-    watch = [];
-    if ($('#' + year).prop('checked')) {
-        watches.forEach(function(item, index) {
-            if (parseInt(item.year) < year) {
-                watch.push(item);
-            }
-        });
-        renderContent(watch);
-    } else {
-        renderContent(watches);
-    }
+	watch = [];
+	if ($('#' + year).prop('checked')) {
+		watches.forEach(function(item, index) {
+			if (parseInt(item.year) < year) {
+				watch.push(item);
+			}
+		});
+		renderContent(watch);
+	} else {
+		renderContent(watches);
+	}
 }
 
 function renderContentByYear2() {
-    watch = [];
-    if ($('#1970').prop('checked')) {
-        watches.forEach(function(item, index) {
-            if (parseInt(item.year) < 1979 && parseInt(item.year) >= 1960) {
-                watch.push(item);
-            }
-        });
-        renderContent(watch);
-    } else {
-        renderContent(watches);
-    }
+	watch = [];
+	if ($('#1970').prop('checked')) {
+		watches.forEach(function(item, index) {
+			if (parseInt(item.year) < 1979 && parseInt(item.year) >= 1960) {
+				watch.push(item);
+			}
+		});
+		renderContent(watch);
+	} else {
+		renderContent(watches);
+	}
 }
 
 function renderContentByYear3() {
-    watch = [];
-    if ($('#1980').prop('checked')) {
-        watches.forEach(function(item, index) {
-            if (parseInt(item.year) < 1999 && parseInt(item.year) >= 1980) {
-                watch.push(item);
-            }
-        });
-        renderContent(watch);
-    } else {
-        renderContent(watches);
-    }
+	watch = [];
+	if ($('#1980').prop('checked')) {
+		watches.forEach(function(item, index) {
+			if (parseInt(item.year) < 1999 && parseInt(item.year) >= 1980) {
+				watch.push(item);
+			}
+		});
+		renderContent(watch);
+	} else {
+		renderContent(watches);
+	}
 }
 
 // FUNCTION TO RENDER CONTENT AFTER FILTER BY PRICE
 function renderContentByPrice1() {
-    watch = [];
-    if ($('#20m').prop('checked')) {
-        watches.forEach(function(item, index) {
-            if (formatString(item.price) < 20000000) {
-                watch.push(item);
-            }
-        });
-        renderContent(watch);
-    } else {
-        renderContent(watches);
-    }
+	watch = [];
+	if ($('#20m').prop('checked')) {
+		watches.forEach(function(item, index) {
+			if (formatString(item.price) < 20000000) {
+				watch.push(item);
+			}
+		});
+		renderContent(watch);
+	} else {
+		renderContent(watches);
+	}
 }
 
 function renderContentByPrice2() {
-    watch = [];
-    if ($('#20plus').prop('checked')) {
-        watches.forEach(function(item, index) {
-            if (
-                formatString(item.price) <= 30000000 &&
-                formatString(item.price) >= 20000000
-            ) {
-                watch.push(item);
-            }
-        });
-        renderContent(watch);
-    } else {
-        renderContent(watches);
-    }
+	watch = [];
+	if ($('#20plus').prop('checked')) {
+		watches.forEach(function(item, index) {
+			if (
+				formatString(item.price) <= 30000000 &&
+				formatString(item.price) >= 20000000
+			) {
+				watch.push(item);
+			}
+		});
+		renderContent(watch);
+	} else {
+		renderContent(watches);
+	}
 }
 
 function renderContentByPrice3() {
-    watch = [];
-    if ($('#30plus').prop('checked')) {
-        watches.forEach(function(item, index) {
-            if (formatString(item.price) > 30000000) {
-                watch.push(item);
-            }
-        });
-        renderContent(watch);
-    } else {
-        renderContent(watches);
-    }
+	watch = [];
+	if ($('#30plus').prop('checked')) {
+		watches.forEach(function(item, index) {
+			if (formatString(item.price) > 30000000) {
+				watch.push(item);
+			}
+		});
+		renderContent(watch);
+	} else {
+		renderContent(watches);
+	}
 }
 
 // FUNCTION TO SORT ITEM BY PRICE
 clone_watches = JSON.parse(JSON.stringify(watches));
 function sortByPriceAsc() {
-    $('.custom-select').change(function() {
-        if (
-            $(this)
-                .children('option:selected')
-                .val() === 'low'
-        ) {
-            clone_watches.sort((a, b) =>
-                formatString(a.price) > formatString(b.price) ? 1 : -1
-            );
-            renderContent(clone_watches);
-        }
-    });
+	$('.custom-select').change(function() {
+		if (
+			$(this)
+				.children('option:selected')
+				.val() === 'low'
+		) {
+			clone_watches.sort((a, b) =>
+				formatString(a.price) > formatString(b.price) ? 1 : -1
+			);
+			renderContent(clone_watches);
+		}
+	});
 }
 
 function sortByPriceDesc() {
-    $('.custom-select').change(function() {
-        if (
-            $(this)
-                .children('option:selected')
-                .val() === 'high'
-        ) {
-            clone_watches.sort((a, b) =>
-                formatString(a.price) < formatString(b.price) ? 1 : -1
-            );
-            renderContent(clone_watches);
-        }
-    });
+	$('.custom-select').change(function() {
+		if (
+			$(this)
+				.children('option:selected')
+				.val() === 'high'
+		) {
+			clone_watches.sort((a, b) =>
+				formatString(a.price) < formatString(b.price) ? 1 : -1
+			);
+			renderContent(clone_watches);
+		}
+	});
 }
 
 function sortByPriceNormal() {
-    $('.custom-select').change(function() {
-        if (
-            $(this)
-                .children('option:selected')
-                .val() === 'normal'
-        ) {
-            renderContent(watches);
-        }
-    });
+	$('.custom-select').change(function() {
+		if (
+			$(this)
+				.children('option:selected')
+				.val() === 'normal'
+		) {
+			renderContent(watches);
+		}
+	});
+}
+
+// CLICK LOCSP TO SHOW AND HIDE FILTER
+function showFilter() {
+	$('.select-left').click(function() {
+		$('#filter').toggle('slow');
+	});
 }
