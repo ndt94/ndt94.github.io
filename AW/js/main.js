5b1e3485f0a0324db28765fd4f8198e71e49a10e
jessen <ndt94hvnh@gmail.com>
2019-05-29 16:23:07 +0700

    localizing & fix remaining bug

--- AW/js/main.js
+++ AW/js/main.js
@@ -1,3 +1,7 @@
+$(function() {
+	cartItem();
+});
+
 // FUNCTION TO RENDER HEADER + FOOTER
 function includeHTML() {
 	var z, i, elmnt, file, xhttp;
@@ -208,4 +212,21 @@ function containObject(array, object) {
 		return true;
 	}
 	return false;
+}
+
+// FUNCTION TO ADD TOTAL CART ITEM NEXT TO CART ICON IN HEADER & BOTTOM OF INDEX PAGE
+function cartItem() {
+	itemsArray = JSON.parse(localStorage.getItem('item'));
+	if (itemsArray !== null) {
+		htmlContent = `<i class="fas fa-shopping-cart"> (${itemsArray.length ||
+			0}) </i>`;
+		$('.fa-shopping-cart')
+			.parent()
+			.html(htmlContent);
+	} else {
+		htmlContent = `<i class="fas fa-shopping-cart"> (0) </i>`;
+		$('.fa-shopping-cart')
+			.parent()
+			.html(htmlContent);
+	}
 }
